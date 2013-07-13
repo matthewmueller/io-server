d60758cf087e3e7a993645434af492b0a11e246d
Matt Mueller <mattmuelle@gmail.com>
2013-07-13 02:54:32 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+/**
+ * Module Dependencies
+ */
+
+var Emitter = require('emitter-component');
+var pool = require('./lib/pool')();
+var slice = [].slice;
+
+/**
+ * Export `IO`
+ */
+
+module.exports = IO;
+
+/**
+ * Mixin `Emitter` on static IO
+ */
+
+Emitter(IO);
+
+/**
+ * Clients
+ */
+
+var clients = [];
+
+/**
+ * Initialize `IO`
+ */
+
+function IO(socket) {
+  if (!(this instanceof IO)) return new IO(socket);
+  this.socket = socket;
+
+  var req = socket.transport.request;
+  var path = this.path = req.query.pathname;
+
+  if (path) pool.push(path, socket);
+  clients.push(socket);
+
+  // handle incoming messages
+  socket.on('message', this.message.bind(this));
+
+  // cleanup
+  socket.on('close', function() {
+    pool.remove(path, socket);
+  });
+}
+
+/**
+ * Called when a message is recieved
+ *
+ * @param {Object} message
+ * @return {IO}
+ */
+
+IO.prototype.message = function(message) {
+  var self = this;
+  var args = slice.call(arguments);
+  var json = JSON.parse(message);
+  var event = json.event;
+  var path = this.path;
+  if (!event) return this;
+
+  if (IO.hasListeners(event)) {
+    return IO.emit.call(this, json, this);
+  } else {
+    this.broadcast(json);
+  }
+};
+
+/**
+ * Send a message to socket
+ */
+
+IO.prototype.emit = function(event, json) {
+  json.event = event;
+  this.broadcast(json);
+  return this;
+};
+
+/**
+ * Broadcast
+ */
+
+IO.prototype.broadcast = function(json) {
+  var path = this.path;
+  var message = JSON.stringify(json);
+
+  // either pull from pool or send to all
+  var sockets = (path) ? pool.pull(path) : clients;
+
+  for (var i = 0, socket; socket = sockets[i]; i++) {
+    socket.send(message);
+  }
+
+  return this;
+};
---
+++ test.js
@@ -0,0 +1,45 @@
+/**
+ * Module Dependencies
+ */
+
+var express = require('express'),
+    engine = require('engine.io'),
+    IO = require('./'),
+    app = express(),
+    es = new engine.Server(),
+    server = require('http').createServer(app);
+
+/**
+ * Handle the upgrade
+ */
+
+server.on('upgrade', function(req, socket, head) {
+  es.handleUpgrade(req, socket, head);
+});
+
+/**
+ * Configuration
+ */
+
+app.configure(function() {
+  app.use(express.logger('dev'));
+  app.use(express.query());
+  app.use('/engine.io', es.handleRequest.bind(es));
+  app.use(express.errorHandler());
+});
+
+/**
+ * Handle the connection
+ */
+
+es.on('connection', IO);
+
+/**
+ * Listen if we are calling this file directly
+ */
+
+if(!module.parent) {
+  var port = process.argv[2] || 9777;
+  server.listen(port);
+  console.log('Server started on port', port);
+}
